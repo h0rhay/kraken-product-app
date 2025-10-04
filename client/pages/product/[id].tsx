641f5c5191fe8dbb45a9ef1fe17df7b54c8e69b2
George Clark <george.clark@holidayextras.co.uk>
2025-10-04 18:45:58 +0100

    feat: Further enhance the product page structure and styling, introduce shopping basket component

--- client/pages/product/[id].tsx
+++ client/pages/product/[id].tsx
@@ -1,46 +1,61 @@
 import { type Product } from "../../types/Product";
 import { GET_PRODUCT_BY_ID } from "../../queries/getProducts";
-
+import ShoppingBasket from "../../components/ShoppingBasket";
 
 export default function Product({ product }: { product: Product }) {
 
-  return <div className="product-page">
-    <figure>
-          <img
-            src="https://static.octopuscdn.com/logos/logo.svg"
-            alt="Octopus Energy Logo"
-          />
-        </figure>
-    {product.img_url && <img src={product.img_url} alt={product.name} />}
+  return(
+    <div className="product-page">
+      <header className="product-header">
+        <img className="product-logo" src="https://static.octopuscdn.com/logos/logo.svg" alt="Octopus Energy Logo" />
+        <ShoppingBasket className="cart-icon" />
+      </header>
 
-    <div>
-      <h1>{product.name}</h1>
-      <p>{product.power} / {product.quantity}</p>
-    </div>
-    <div>
-      <p>{product.price}</p>
-      <button>+</button>
-      <button>Quantity</button>
-      <button>-</button>
-    </div>
-    <div>
-    <button>Add to cart</button>
-    </div>
-    <div>
+      <figure className="product-image">
+        {product.img_url && <img src={product.img_url} alt={product.name} />}
+      </figure>
 
-    <h2>Description</h2>
-    <p>{product.description}</p>
-    </div>
-    <div>  
-    <h2>Specifications</h2>
-    <p>Brand: {product.brand}</p>
-    <p>Weight: {product.weight} kg</p>
-    <p>Dimensions: {product.height} x {product.width} x {product.length} cm</p>
-    <p>Model code: {product.model_code}</p>
-    <p>Colour: {product.colour}</p>
-    </div>
+      <div className="product-title">
+        <h1>{product.name}</h1>
+        <p className="product-specs">{product.power} // {product.quantity}</p>
+      </div>
 
-  </div>;
+      <div className="price-qty">
+        <h1>£{product.price.toFixed(2)}</h1>
+        <div className="qty-wrapper">
+          <p className="qty-label">Qty</p>
+          <div className="qty-controls">
+            <button className="qty-btn">-</button>
+            <span>1</span>
+            <button className="qty-btn plus">+</button>
+          </div>
+        </div>
+      </div>
+
+      <button className="add-to-cart">Add to cart</button>
+
+      <section>
+        <h2>Description</h2>
+        <p className="product-description">{product.description}</p>
+      </section>
+
+      <section>
+        <h2>Specifications</h2>
+        <div className="specs">
+          <span className="spec-label">Brand</span>
+          <span className="spec-value">{product.brand}</span>
+          <span className="spec-label">Item weight (g)</span>
+          <span className="spec-value">{product.weight}</span>
+          <span className="spec-label">Dimensions (cm)</span>
+          <span className="spec-value">{product.height} x {product.width} x {product.length}</span>
+          <span className="spec-label">Item Model number</span>
+          <span className="spec-value">{product.model_code}</span>
+          <span className="spec-label">Colour</span>
+          <span className="spec-value">{product.colour}</span>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export const getStaticProps = async ({ params }: { params: { id: string } }) => {
